800f2a6e0782b8770fca4f9d47df8b889411f34f
Jeevithayd <143778098+Jeevithayd@users.noreply.github.com>
2024-03-21 19:43:40 +0530

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let message = document.getElementById("message");
-/*let messageOnline = () => {
+let messageOnline = () => {
     message.textContent="Internet Connection Available";
     message.style.cssText="background-color:blue; color:dark-blue;";
-}; */
+};
 let messageOffline = () => {
     message.textContent="No Internet Connection";
     message.style.cssText="background-color:red; color:dark-red;";
 };
 if(window.navigator.onLine){
-    messageOffline();
+    messageOnline();
 }
 else{
     messageOffline();
